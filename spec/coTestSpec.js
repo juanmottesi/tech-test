d9555014d67f65f1bac46964058ee13bea2fe3b3
Juan Pablo Mottesi <jmottesi@portfoliopersonal.com>
2023-07-12 23:54:07 -0300

    feat: se agregan test para Low Coverage

--- spec/coTestSpec.js
+++ spec/coTestSpec.js
@@ -4,12 +4,53 @@ const coTest = require('../src/coTest');
 const CarInsurance = coTest.CarInsurance;
 const Product = coTest.Product;
 
-describe("Co Test", function() {
+describe("Low Coverage", function() {
 
-  it("should foo", function() {
-    const coTest = new CarInsurance([ new Product("foo", 0, 0) ]);
+  it("should be down price by 1 when sellIn > 0", function() {
+    const coTest = new CarInsurance([new Product("Low Coverage", 1, 10) ]);
     const products = coTest.updatePrice();
-    expect(products[0].name).equal("fixme");
+    expect(products[0].name).equal("Low Coverage");
+    expect(products[0].sellIn).equal(0);
+    expect(products[0].price).equal(9);
   });
 
+  it("should be down price by 2 when sellIn = 0", function() {
+    const coTest = new CarInsurance([new Product("Low Coverage", 0, 10) ]);
+    const products = coTest.updatePrice();
+    expect(products[0].name).equal("Low Coverage");
+    expect(products[0].sellIn).equal(-1);
+    expect(products[0].price).equal(8);
+  });
+
+  it("should be down price by 2 when sellIn < 0", function() {
+    const coTest = new CarInsurance([new Product("Low Coverage", -1, 10) ]);
+    const products = coTest.updatePrice();
+    expect(products[0].name).equal("Low Coverage");
+    expect(products[0].sellIn).equal(-2);
+    expect(products[0].price).equal(8);
+  });
+
+  it("should price never go negative when sellIn > 0", function() {
+    const coTest = new CarInsurance([new Product("Low Coverage", 1, 0) ]);
+    const products = coTest.updatePrice();
+    expect(products[0].name).equal("Low Coverage");
+    expect(products[0].sellIn).equal(0);
+    expect(products[0].price).equal(0);
+  });
+
+  it("should price never go negative when sellIn = 0", function() {
+    const coTest = new CarInsurance([new Product("Low Coverage", 0, 0) ]);
+    const products = coTest.updatePrice();
+    expect(products[0].name).equal("Low Coverage");
+    expect(products[0].sellIn).equal(-1);
+    expect(products[0].price).equal(0);
+  });
+
+  it("should price never go negative when sellIn < 0", function() {
+    const coTest = new CarInsurance([new Product("Low Coverage", -1, 0) ]);
+    const products = coTest.updatePrice();
+    expect(products[0].name).equal("Low Coverage");
+    expect(products[0].sellIn).equal(-2);
+    expect(products[0].price).equal(0);
+  });
 });
