40021f550701c2d7e20357419bd23585245e5d63
Juan Pablo Mottesi <jmottesi@portfoliopersonal.com>
2023-07-13 00:10:36 -0300

    feat: se agregan test para Special Full Coverage

--- spec/coTestSpec.js
+++ spec/coTestSpec.js
@@ -115,5 +115,104 @@ describe("Mega Coverage", function () {
     expect(products[0].sellIn).equal(1);
     expect(products[0].price).equal(80);
   });
+});
+
+describe("Special Full Coverage", function () {
+
+  it("should be increase price by 1 when sellIn > 10", function () {
+    const coTest = new CarInsurance([new Product("Special Full Coverage", 11, 10)]);
+    const products = coTest.updatePrice();
+    expect(products[0].name).equal("Special Full Coverage");
+    expect(products[0].sellIn).equal(10);
+    expect(products[0].price).equal(11);
+  });
+
+  it("should be increase price by 2 when sellIn = 10", function () {
+    const coTest = new CarInsurance([new Product("Special Full Coverage", 10, 10)]);
+    const products = coTest.updatePrice();
+    expect(products[0].name).equal("Special Full Coverage");
+    expect(products[0].sellIn).equal(9);
+    expect(products[0].price).equal(12);
+  });
+  
+  it("should be increase price by 2 when 5 > sellIn < 10", function () {
+    const coTest = new CarInsurance([new Product("Special Full Coverage", 7, 10)]);
+    const products = coTest.updatePrice();
+    expect(products[0].name).equal("Special Full Coverage");
+    expect(products[0].sellIn).equal(6);
+    expect(products[0].price).equal(12);
+  });
+  
+  it("should be increase price by 3 when sellIn = 5", function () {
+    const coTest = new CarInsurance([new Product("Special Full Coverage", 5, 10)]);
+    const products = coTest.updatePrice();
+    expect(products[0].name).equal("Special Full Coverage");
+    expect(products[0].sellIn).equal(4);
+    expect(products[0].price).equal(13);
+  });
+  
+  it("should be increase price by 3 when 0 > sellIn < 5", function () {
+    const coTest = new CarInsurance([new Product("Special Full Coverage", 3, 10)]);
+    const products = coTest.updatePrice();
+    expect(products[0].name).equal("Special Full Coverage");
+    expect(products[0].sellIn).equal(2);
+    expect(products[0].price).equal(13);
+  });
+  
+  it("should be price = 0 when sellIn = 0", function () {
+    const coTest = new CarInsurance([new Product("Special Full Coverage", 0, 10)]);
+    const products = coTest.updatePrice();
+    expect(products[0].name).equal("Special Full Coverage");
+    expect(products[0].sellIn).equal(-1);
+    expect(products[0].price).equal(0);
+  });
+
+  it("should be price = 0 when sellIn < 0", function () {
+    const coTest = new CarInsurance([new Product("Special Full Coverage", -1, 10)]);
+    const products = coTest.updatePrice();
+    expect(products[0].name).equal("Special Full Coverage");
+    expect(products[0].sellIn).equal(-2);
+    expect(products[0].price).equal(0);
+  });
+
+  it("should never increase price when price = 50 and sellIn > 10", function () {
+    const coTest = new CarInsurance([new Product("Special Full Coverage", 11, 50)]);
+    const products = coTest.updatePrice();
+    expect(products[0].name).equal("Special Full Coverage");
+    expect(products[0].sellIn).equal(10);
+    expect(products[0].price).equal(50);
+  });
+
+  it("should never increase price when price = 50 and sellIn = 10", function () {
+    const coTest = new CarInsurance([new Product("Special Full Coverage", 10, 50)]);
+    const products = coTest.updatePrice();
+    expect(products[0].name).equal("Special Full Coverage");
+    expect(products[0].sellIn).equal(9);
+    expect(products[0].price).equal(50);
+  });
+
+  it("should never increase price when price = 50 and 5 > sellIn < 10", function () {
+    const coTest = new CarInsurance([new Product("Special Full Coverage", 7, 50)]);
+    const products = coTest.updatePrice();
+    expect(products[0].name).equal("Special Full Coverage");
+    expect(products[0].sellIn).equal(6);
+    expect(products[0].price).equal(50);
+  });
+
+  it("should never increase price when price = 50 and sellIn = 5", function () {
+    const coTest = new CarInsurance([new Product("Special Full Coverage", 5, 50)]);
+    const products = coTest.updatePrice();
+    expect(products[0].name).equal("Special Full Coverage");
+    expect(products[0].sellIn).equal(4);
+    expect(products[0].price).equal(50);
+  });
+
+  it("should never increase price when price = 50 and 0 > sellIn < 5", function () {
+    const coTest = new CarInsurance([new Product("Special Full Coverage", 3, 50)]);
+    const products = coTest.updatePrice();
+    expect(products[0].name).equal("Special Full Coverage");
+    expect(products[0].sellIn).equal(2);
+    expect(products[0].price).equal(50);
+  });
 
 });
